5877bfe77fb5dd0438c839aa7ba4f188a88af0fb
Nuno512 <Nuno_512@outlook.com>
2021-11-27 19:02:11 +0000

    Rules

--- web-app/src/components/GeneralData.vue
+++ web-app/src/components/GeneralData.vue
@@ -1,5 +1,7 @@
 <template>
+
   <div>
+    <span class="border border-bottom-0"></span>
     <div class="container-fluid">
       <div class="row">
         <div class="col-md">
@@ -11,30 +13,32 @@
                   <div class="card-body">
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-temperature-high text-dark pe-1"></i>
+                        <i class="fas fa-temperature-high text-dark"></i>
+                        <i class="pe-1"></i>
                         <strong> Temperatura </strong>
                         <div id="currentTemp" class="ms-auto"></div>
                       </div>
                     </div>
                     <div>
                       <div class="d-flex">
-                        <i
-                          class="fas fa-temperature-high text-warning pe-1"
-                        ></i>
+                        <i class="fas fa-temperature-high text-warning"></i>
+                        <i class="pe-1"></i>
                         <strong> Sensação térmica </strong>
                         <div id="currentTemp" class="ms-auto"></div>
                       </div>
                     </div>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-temperature-high text-danger pe-1"></i>
+                        <i class="fas fa-temperature-high text-danger"></i>
+                        <i class="pe-1"></i>
                         <strong> Temperatura máxima </strong>
                         <div id="maxTemp" class="ms-auto"></div>
                       </div>
                     </div>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-temperature-low text-primary pe-1"></i>
+                        <i class="fas fa-temperature-low text-primary"></i>
+                        <i class="pe-1"></i>
                         <strong> Temperatura mínima</strong>
                         <div id="minTemp" class="ms-auto"></div>
                       </div>
@@ -45,28 +49,32 @@
                   <div class="card-body">
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-sun text-warning pe-1"></i>
+                        <i class="fas fa-sun text-warning"></i>
+                        <i class="pe-1"></i>
                         <strong> Nascer do sol </strong>
                         <div id="sunriseTime" class="ms-auto"></div>
                       </div>
                     </div>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-moon text-black pe-1"></i>
+                        <i class="fas fa-moon text-black"></i>
+                        <i class="pe-1"></i>
                         <strong> Pôr do sol </strong>
                         <div id="sunsetTime" class="ms-auto"></div>
                       </div>
                     </div>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-percentage text-info pe-0"></i>
+                        <i class="fas fa-percentage text-info"></i>
+                        <i class="pe-1"></i>
                         <strong> Humidade </strong>
                         <div id="fetchedTime" class="ms-auto"></div>
                       </div>
                     </div>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-cloud text-info pe-1"></i>
+                        <i class="fas fa-cloud text-info"></i>
+                        <i class="pe-1"></i>
                         <strong> Nebulosidade </strong>
                         <div id="fetchedTime" class="ms-auto"></div>
                       </div>
@@ -77,28 +85,32 @@
                   <div class="card-body">
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-wind text-muted pe-1"></i>
+                        <i class="fas fa-wind text-muted"></i>
+                        <i class="pe-1"></i>
                         <strong> Velocidade do vento </strong>
                         <div id="sunriseTime" class="ms-auto"></div>
                       </div>
                     </div>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-wind text-muted pe-1"></i>
+                        <i class="fas fa-wind text-muted"></i>
+                        <i class="pe-1"></i>
                         <strong> Sentido do vento </strong>
                         <div id="sunsetTime" class="ms-auto"></div>
                       </div>
                     </div>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-cloud-rain text-info pe-1"></i>
+                        <i class="fas fa-cloud-rain text-info"></i>
+                        <i class="pe-1"></i>
                         <strong> Volume de chuva na última hora </strong>
                         <div id="fetchedTime" class="ms-auto"></div>
                       </div>
                     </div>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-snowflake text-primary pe-1"></i>
+                        <i class="fas fa-snowflake text-primary"></i>
+                        <i class="pe-1"></i>
                         <strong> Volume de neve na última hora </strong>
                         <div id="fetchedTime" class="ms-auto"></div>
                       </div>
@@ -111,7 +123,7 @@
         </div>
       </div>
     </div>
-
+    <span class="border border-bottom-0"></span>
     <div class="container-fluid">
       <div class="row">
         <div class="col-md">
@@ -123,7 +135,8 @@
                   <div class="card-body">
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-hospital-user text-danger pe-1"></i>
+                        <i class="fas fa-hospital-user text-danger"></i>
+                        <i class="pe-1"></i>
                         <strong>
                           Casos confirmados nos últimos 14 dias em Leiria
                         </strong>
@@ -132,7 +145,8 @@
                     </div>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-hospital-user text-danger pe-1"></i>
+                        <i class="fas fa-hospital-user text-danger"></i>
+                        <i class="pe-1"></i>
                         <strong>
                           Casos comfirmados no último dia em Leiria
                         </strong>
@@ -147,7 +161,7 @@
         </div>
       </div>
     </div>
-
+    <span class="border border-bottom-0"></span>
     <div class="container-fluid">
       <div class="row">
         <div class="col-md">
@@ -162,21 +176,24 @@
                     <h5 class="card-title text-center">Estufa</h5>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-thermometer-half pe-1"></i>
+                        <i class="fas fa-thermometer-half"></i>
+                        <i class="pe-1"></i>
                         <strong> Temperatura </strong>
                         <div id="currentTemp" class="ms-auto"></div>
                       </div>
                     </div>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-percentage text-info pe-1"></i>
+                        <i class="fas fa-percentage text-info"></i>
+                        <i class="pe-1"></i>
                         <strong> Humidade do ar </strong>
                         <div id="currentTemp" class="ms-auto"></div>
                       </div>
                     </div>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-percentage text-info pe-1"></i>
+                        <i class="fas fa-percentage text-info"></i>
+                        <i class="pe-1"></i>
                         <strong> Humidade do solo </strong>
                         <div id="maxTemp" class="ms-auto"></div>
                       </div>
@@ -188,21 +205,24 @@
                     <h5 class="card-title text-center">Jardim da Piscina</h5>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-thermometer-half pe-1"></i>
+                        <i class="fas fa-thermometer-half"></i>
+                        <i class="pe-1"></i>
                         <strong> Temperatura </strong>
                         <div id="currentTemp" class="ms-auto"></div>
                       </div>
                     </div>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-sun text-warning pe-1"></i>
+                        <i class="fas fa-sun text-warning"></i>
+                        <i class="pe-1"></i>
                         <strong> Exposição solar </strong>
                         <div id="currentTemp" class="ms-auto"></div>
                       </div>
                     </div>
                     <div>
                       <div class="d-flex">
-                        <i class="fas fa-percentage text-info pe-1"></i>
+                        <i class="fas fa-percentage text-info"></i>
+                        <i class="pe-1"></i>
                         <strong> Humidade do solo </strong>
                         <div id="maxTemp" class="ms-auto"></div>
                       </div>
